b21d9dc25861c3d57b1faf50e4de4f5cdb9381ca
Roberto <roberto.menesantos@gmail.com>
2025-08-30 20:49:49 -0300

    refactor: cleaning the code

--- client/main.py
+++ client/main.py
@@ -1,12 +1,10 @@
 from sys import argv
 
 from app.core import cli_action, interface_create
-from app.core import cli_action, interface_create
 
 
 def main():
     if len(argv) <= 1:
-        interface_create()
         interface_create()
         return
     elif len(argv) > 3:
@@ -16,7 +14,6 @@ def main():
         argv[1],
         argv[2] if len(argv) == 3 else None,
     )
-
 
     print("All seals registered")
 
